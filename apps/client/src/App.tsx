85d436ff9eb0957f5f4a88f9db67c0342dfcea1b
surimi <jorge.cobreros@protonmail.com>
2023-05-24 13:36:59 +0200

    redux

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
 
 function App() {
   const [greeting, setGreeting] = useState('')
+
 
   useEffect(() => {
     fetch('/api')
@@ -12,18 +12,15 @@ function App() {
       .then(setGreeting)
   }, []);
 
+
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>{greeting}</h1>
-      
+      <div>
+
+      </div>
     </div>
   )
 }
